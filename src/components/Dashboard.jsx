f0b1c9019a8de8dc9fe67e15e3e2de99fa699395
unknown <azhar.mehic@gmail.com>
2023-02-02 20:10:38 +0100

    first commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+
+const Dashboard = () => <h1>Dashboard</h1>
+
+export default Dashboard;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  App
+} from "./components";
+import Dashboard from "./components/Dashboard";
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </Router>,
+
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
