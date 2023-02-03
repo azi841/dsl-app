d85b06a75ce753462ee73145b4137f9ddfb45160
unknown <azhar.mehic@gmail.com>
2023-02-03 21:58:56 +0100

    Scrollable list with search filter, in development

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,17 +8,35 @@ import ReactScrollableList from './dist/index'
 
 
 let people =[];
-const user = "User";
+const strUser = "User";
 
 for(let i = 0; i<100;i++){
     let num = i+1;
     let str = num.toString();    
-    people.push({id:i, content:user+str})
+    people.push({id:i, content:strUser+str})
 }
 
 const Dashboard = () => {
     
-  
+    const [person, setPerson] = useState('');
+    const [foundPerson, setFoundPerson] = useState(people);
+
+    const filter = (e) => {
+        const keyword = e.target.value;
+
+        if (keyword !== '') {
+            const results = people.filter((user) => {
+                return user.value.toLowerCase().startsWith(keyword.toLowerCase());
+            });
+            setFoundPerson(results);
+        } else{
+            setFoundPerson(people);
+        }
+
+        setPerson(keyword);
+    }
+    
+
     const navigate = useNavigate();
     return(
         <>
@@ -28,19 +46,32 @@ const Dashboard = () => {
                 </div>
                 <div className="Search">
                     <input 
-                    type ="text"
+                    type ="search"
+                    value={person}
+                    onChange={filter}
+                    className="input"
                     placeholder="Search"
                     />
                     {/*
                     <ul>
                     {people.map((people) => <li key={people.id}>{people.username}</li>)}
                     </ul>*/}
-                    <ReactScrollableList
-                    listItems={people}
-                    heightOfItem={30}
-                    maxItemsToRender={20}
-                    style={{ color: '#333' }}
-                    />
+                    <div>
+                        <ReactScrollableList
+                        {foundPerson&&foundPerson.length > 0 ?(
+                            foundPerson.map((person)=>(
+                                
+                                listItems={people}
+                                heightOfItem={30}
+                                maxItemsToRender={20}
+                                style={{ color: '#333' }}
+                                
+                            ))
+                        ) : (
+                            <h1>No results found!</h1>
+                        )}
+                        />
+                    </div>
                 </div>
                 <div>
                     <button className="Button" onClick={() => navigate(-1)}>Log out</button>
