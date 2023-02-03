484fcd7540f3c81fbf8b34d83ca1de83be05f71d
unknown <azhar.mehic@gmail.com>
2023-02-03 16:22:55 +0100

    Contact list

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
+import React from "react";
 
 import "./Dashboard.css";
 
+const people =[
+  "User1",
+  "User2",
+  "User3",
+  "User4"
+];
 
 const Dashboard = (props) => {
-
+    
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
+    const handleChange = event => {
+       setSearchTerm(event.target.value);
+    }
+    React.useEffect(() => {
+        const results = people.filter(person =>
+            person.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setSearchResults(results);
+        }, [searchTerm]);
     const navigate = useNavigate();
     return(
         <>
             <div className="Box">
                 <div className="Title">
                     <h1>Dashboard</h1>
+                </div>
+                <div className="Search">
+                    <input 
+                    type ="text"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={handleChange}
+                    />
+                    <ul>
+                        {searchResults.map(item => (
+                            <li>{item}</li>
+                        ))}
+                    </ul>
                 </div>
                 <div>
                     <button className="Button" onClick={() => navigate(-1)}>Log out</button>
