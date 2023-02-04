6c23090118c2b0460226448083bf4df626805034
unknown <azhar.mehic@gmail.com>
2023-02-04 18:02:44 +0100

    Data update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,44 +7,15 @@ import "./Dashboard.css";
 import ReactScrollableList from './dist/index'
 
 
-let people =[
-    {
-        id:1, 
-        uname: "User1"
-    },
-    {
-        id:2, 
-        uname:"User2"
-    },
-    {
-        id:3, 
-        uname:"User3"
-    },
-    {
-        id:4, 
-        uname:"User4"
-    },
-    {
-        id:5,
-        uname:"User5"
-    },
-    {
-        id:6,
-        uname:"User6"
-    },
-    {
-        id:7,
-        uname:"User7"
-    }
-];
+let people =[];
 const strUser = "User";
 
-/*for(let i = 0; i<100;i++){
+for(let i = 0; i<100;i++){
     let num = i+1;
     let str = num.toString();    
-    people.push({id:i, content:strUser+str})
+    people.push({id:i, uname:strUser+str})
 }
-*/
+
 
 const Dashboard = () => {
     
